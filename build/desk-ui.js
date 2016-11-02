0209bc2997b54ce5dbbb467d7833b1833cfb0800
hgs <zohegs@gmail.com>
2016-11-02 23:46:41 +0800

    the three

--- build/desk-ui.js
+++ build/desk-ui.js
@@ -1,6 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
+    Vue.component("di-icon", {
+        template: "<span class=\"di-icon\" :class=\"classObject\" :style=\"styleObject\"></span>",
+        props: ["margin",
+            "icon"],
+        computed: {
+            classObject: function () {
+                return "di-icon--" + this.icon;
+            },
+            styleObject: function () {
+                return {
+                    "margin": this.margin && this.margin.replace(/ /g, "px ") + "px"
+                };
+            }
+        }
+    });
+    Vue.component("di-layout", {
+        template: "<div class=\"di-layout\" :style=\"styleObject\"><slot></div>",
+        props: ["margin", "flex", "padding", "radius", "opacity",
+            "bgcolor", "direction", "type", "color"],
+        computed: {
+            styleObject: function () {
+                return {
+                    "margin": this.margin && this.margin.replace(/ /g, "px ") + "px",
+                    "flex": this.flex,
+                    "padding": this.padding && this.padding.replace(/ /g, "px ") + "px",
+                    "border-radius": this.radius && this.radius.replace(/ /g, "px ") + "px",
+                    "opacity": this.opacity,
+                    "background-color": this.bgcolor,
+                    "flex-direction": (this.direction === "vertical" || this.direction === "v") && "column",
+                    "display": this.type,
+                    "color": this.color
+                };
+            }
+        }
+    });
     Vue.component("di-button", {
-        template: "<button class=\"di-button\"><slot></button>"
+        template: "<button class=\"di-button\" :class=\"classObject\" :style=\"styleObject\"><span v-if=\"icon\" :class=\"['di-icon', 'di-icon--' + icon]\" style=\"margin-right: 5px;\"></span><slot></button>",
+        props: ["margin", "flex", "padding", "radius", "opacity",
+            "type", "icon"],
+        computed: {
+            classObject: function () {
+                return {
+                    "di-button--primary": this.type === "primary"
+                };
+            },
+            styleObject: function () {
+                return {
+                    "margin": this.margin && this.margin.replace(/ /g, "px ") + "px",
+                    "flex": this.flex,
+                    "padding": this.padding && this.padding.replace(/ /g, "px ") + "px",
+                    "border-radius": this.radius && this.radius.replace(/ /g, "px ") + "px",
+                    "opacity": this.opacity
+                };
+            },
+            iconDom: function () {
+                if (this.icon) {
+                    return "<span class=\"di-icon di-icon--" + this.icon + "\" style=\"margin-right: 5px;\"></span>";
+                }
+                return "";
+            }
+        }
     });
     new Vue({
         el: "#desk-ui",
--- desk-ui.ts
+++ desk-ui.ts
@@ -1,7 +1,70 @@
 document.addEventListener("DOMContentLoaded", function(): void {
+    /* --- Icon --- */
+    Vue.component("di-icon", {
+        template: `<span class="di-icon" :class="classObject" :style="styleObject"></span>`,
+        props: ["margin",
+            "icon"],
+        computed: {
+            classObject: function(): Object {
+                return "di-icon--" + this.icon;
+            },
+            styleObject: function(): Object {
+                return {
+                    "margin": this.margin && this.margin.replace(/ /g, "px ") + "px"
+                };
+            }
+        }
+    });
+
+    /* --- Layout --- */
+    Vue.component("di-layout", {
+        template: `<div class="di-layout" :style="styleObject"><slot></div>`,
+        props: ["margin", "flex", "padding", "radius", "opacity",
+            "bgcolor", "direction", "type", "color"],
+        computed: {
+            styleObject: function(): Object {
+                return {
+                    "margin": this.margin && this.margin.replace(/ /g, "px ") + "px",
+                    "flex": this.flex,
+                    "padding": this.padding && this.padding.replace(/ /g, "px ") + "px",
+                    "border-radius": this.radius && this.radius.replace(/ /g, "px ") + "px",
+                    "opacity": this.opacity,
+                    "background-color": this.bgcolor,
+                    "flex-direction": (this.direction === "vertical" || this.direction === "v") && "column",
+                    "display": this.type,
+                    "color": this.color
+                };
+            }
+        }
+    });
+
     /* --- Button --- */
     Vue.component("di-button", {
-        template: `<button class="di-button"><slot></button>`
+        template: `<button class="di-button" :class="classObject" :style="styleObject"><span v-if="icon" :class="['di-icon', 'di-icon--' + icon]" style="margin-right: 5px;"></span><slot></button>`,
+        props: ["margin", "flex", "padding", "radius", "opacity",
+            "type", "icon"],
+        computed: {
+            classObject: function(): Object {
+                return {
+                    "di-button--primary": this.type === "primary"
+                };
+            },
+            styleObject: function(): Object {
+                return {
+                    "margin": this.margin && this.margin.replace(/ /g, "px ") + "px",
+                    "flex": this.flex,
+                    "padding": this.padding && this.padding.replace(/ /g, "px ") + "px",
+                    "border-radius": this.radius && this.radius.replace(/ /g, "px ") + "px",
+                    "opacity": this.opacity
+                };
+            },
+            iconDom: function(): string {
+                if (this.icon) {
+                    return `<span class="di-icon di-icon--${this.icon}" style="margin-right: 5px;"></span>`;
+                }
+                return "";
+            }
+        }
     });
     /* --- Init --- */
     new Vue({
