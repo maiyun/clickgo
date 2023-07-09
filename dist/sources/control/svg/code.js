73b1a02310f964384288906cfd0d23777bb3cc80
yunbookf <zohegs@gmail.com>
2023-07-09 13:50:39 +0800

    Add src param of svg control.

--- dist/sources/control/svg/code.js
+++ dist/sources/control/svg/code.js
@@ -22,6 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const clickgo = __importStar(require("clickgo"));
 class default_1 extends clickgo.control.AbstractControl {
@@ -29,8 +38,46 @@ class default_1 extends clickgo.control.AbstractControl {
         super(...arguments);
         this.props = {
             'viewBox': '0 0 24 24',
-            'layout': ''
+            'layout': '',
+            'src': ''
         };
+        this.fileLayout = '';
+        this.fileViewBox = '';
+        this.count = 0;
+    }
+    onMounted() {
+        this.watch('src', () => __awaiter(this, void 0, void 0, function* () {
+            const count = ++this.count;
+            if (typeof this.props.src !== 'string' || this.props.src === '') {
+                this.fileLayout = '';
+                this.fileViewBox = '';
+                return;
+            }
+            const pre = this.props.src.slice(0, 6).toLowerCase();
+            if (pre === 'file:/' || pre === 'http:/' || pre === 'https:' || pre.startsWith('data:')) {
+                return;
+            }
+            const path = clickgo.tool.urlResolve('/package' + this.path + '/', this.props.src);
+            const blob = yield clickgo.fs.getContent(path);
+            if ((count !== this.count) || !blob || typeof blob === 'string') {
+                return;
+            }
+            const t = yield clickgo.tool.blob2Text(blob);
+            if (count !== this.count) {
+                return;
+            }
+            if (t) {
+                const viewBoxMatch = /<svg[\s\S]*?viewBox\s*?=\s*?"(.+?)"/.exec(t);
+                const layoutMatch = /<svg[\s\S]*?>([\s\S]*?)<\/svg>/.exec(t);
+                this.fileViewBox = viewBoxMatch ? viewBoxMatch[1] : '';
+                this.fileLayout = layoutMatch ? layoutMatch[1] : '';
+                return;
+            }
+            this.fileLayout = '';
+            this.fileViewBox = '';
+        }), {
+            'immediate': true
+        });
     }
 }
 exports.default = default_1;
--- dist/sources/control/svg/code.ts
+++ dist/sources/control/svg/code.ts
@@ -4,7 +4,55 @@ export default class extends clickgo.control.AbstractControl {
 
     public props = {
         'viewBox': '0 0 24 24',
-        'layout': ''
+        /** --- 直接插入 svg 内部标签，有的话优先显示本图形 --- */
+        'layout': '',
+        /** --- 或者从文件中读取 --- */
+        'src': ''
     };
+
+    /** --- 文件中读取的 layout --- */
+    public fileLayout: string = '';
+
+    public fileViewBox: string = '';
+
+    /** --- watch: src 变更次数 --- */
+    public count = 0;
+
+    public onMounted(): void | Promise<void> {
+        this.watch('src', async () => {
+            const count = ++this.count;
+            if (typeof this.props.src !== 'string' || this.props.src === '') {
+                this.fileLayout = '';
+                this.fileViewBox = '';
+                return;
+            }
+            const pre = this.props.src.slice(0, 6).toLowerCase();
+            if (pre === 'file:/' || pre === 'http:/' || pre === 'https:' || pre.startsWith('data:')) {
+                // --- svg 只能从本地读取 ---
+                return;
+            }
+            // --- 本 app 包 ---
+            const path = clickgo.tool.urlResolve('/package' + this.path + '/', this.props.src);
+            const blob = await clickgo.fs.getContent(path);
+            if ((count !== this.count) || !blob || typeof blob === 'string') {
+                return;
+            }
+            const t = await clickgo.tool.blob2Text(blob);
+            if (count !== this.count) {
+                return;
+            }
+            if (t) {
+                const viewBoxMatch = /<svg[\s\S]*?viewBox\s*?=\s*?"(.+?)"/.exec(t);
+                const layoutMatch = /<svg[\s\S]*?>([\s\S]*?)<\/svg>/.exec(t);
+                this.fileViewBox = viewBoxMatch ? viewBoxMatch[1] : '';
+                this.fileLayout = layoutMatch ? layoutMatch[1] : '';
+                return;
+            }
+            this.fileLayout = '';
+            this.fileViewBox = '';
+        }, {
+            'immediate': true
+        });
+    }
 
 }
