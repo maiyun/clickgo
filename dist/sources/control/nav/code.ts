13866cfe4e4ebe490d7bf607bf6cffad5eec6f98
yunbookf <zohegs@gmail.com>
2024-01-20 19:57:14 +0800

    Fix select on mobile.

--- dist/sources/control/nav/code.ts
+++ dist/sources/control/nav/code.ts
@@ -2,6 +2,10 @@ import * as clickgo from 'clickgo';
 
 export default class extends clickgo.control.AbstractControl {
 
+    public emits = {
+        'layer': null
+    };
+
     public props: {
         'modelValue': string;
         // --- pop 模式时是否在显示 ---
