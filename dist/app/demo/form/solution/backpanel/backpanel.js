1fb404214b3cc82ad21e271605b5bbc47b43b5ee
yunbookf <zohegs@gmail.com>
2024-01-22 18:25:32 +0800

    Op.

--- dist/app/demo/form/solution/backpanel/backpanel.js
+++ dist/app/demo/form/solution/backpanel/backpanel.js
@@ -22,17 +22,41 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const clickgo = __importStar(require("clickgo"));
+const test1_1 = __importDefault(require("../../control/panel/test1"));
 class default_1 extends clickgo.form.AbstractForm {
     constructor() {
         super(...arguments);
-        this.fd = '';
+        this.fh = '';
         this.name = '';
+        this.map = {
+            'test1': test1_1.default,
+            'test2': '../../control/panel/test2'
+        };
     }
-    onMounted() {
-        this.watch('formHash', () => {
-            this.fd = this.formHash;
+    onFormHashChange(taskId, formId, value) {
+        if (formId !== this.formId) {
+            return;
+        }
+        this.fh = value;
+    }
+    onSelect(e) {
+        return __awaiter(this, void 0, void 0, function* () {
+            e.preventDefault();
+            yield clickgo.form.dialog('Show form');
         });
     }
 }
--- dist/app/demo/form/solution/backpanel/backpanel.ts
+++ dist/app/demo/form/solution/backpanel/backpanel.ts
@@ -1,15 +1,28 @@
 import * as clickgo from 'clickgo';
+import * as types from '~/types';
+import test1Panel from '../../control/panel/test1';
 
 export default class extends clickgo.form.AbstractForm {
 
-    public fd: string = '';
+    public fh: string = '';
 
     public name: string = '';
 
-    public onMounted(): void {
-        this.watch('formHash', () => {
-            this.fd = this.formHash;
-        });
+    public map: Record<string, any> = {
+        'test1': test1Panel,
+        'test2': '../../control/panel/test2'
+    };
+
+    public onFormHashChange(taskId: number, formId: number, value: string): void {
+        if (formId !== this.formId) {
+            return;
+        }
+        this.fh = value;
+    }
+
+    public async onSelect(e: types.INavItemSelectEvent): Promise<void> {
+        e.preventDefault();
+        await clickgo.form.dialog('Show form');
     }
 
 }
