75f6790f077ceaa2119dc45d47601f5df51e29f8
han <zohegs@gmail.com>
2024-07-12 19:07:47 +0800

    v3.14.5

--- dist/clickgo.ts
+++ dist/clickgo.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const version = '3.14.4';
+const version = '3.14.5';
 export function getVersion(): string {
     return version;
 }
