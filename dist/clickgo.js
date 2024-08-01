5abf756f9ea4d59ed2f7a634249d075691f8f391
han <zohegs@gmail.com>
2024-08-01 14:06:14 +0800

    3.15.13

--- dist/clickgo.js
+++ dist/clickgo.js
@@ -29,7 +29,7 @@ exports.isNative = isNative;
 exports.getPlatform = getPlatform;
 exports.isImmersion = isImmersion;
 exports.hasFrame = hasFrame;
-const version = '3.15.12';
+const version = '3.15.13';
 function getVersion() {
     return version;
 }
--- dist/clickgo.ts
+++ dist/clickgo.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const version = '3.15.12';
+const version = '3.15.13';
 export function getVersion(): string {
     return version;
 }
