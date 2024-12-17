0945d2550454193064105c95a28564acf1204c6c
han <zohegs@gmail.com>
2024-12-17 18:55:30 +0800

    v3.16.1

--- dist/clickgo.js
+++ dist/clickgo.js
@@ -39,7 +39,7 @@ exports.isNative = isNative;
 exports.getPlatform = getPlatform;
 exports.isImmersion = isImmersion;
 exports.hasFrame = hasFrame;
-const version = '3.16.0';
+const version = '3.16.1';
 function getVersion() {
     return version;
 }
