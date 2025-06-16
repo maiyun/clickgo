7adbe6ad2f3d7db40bd6edb773de4a21292d1581
han <zohegs@gmail.com>
2025-06-17 01:33:20 +0800

    3.16.15

--- dist/clickgo.js
+++ dist/clickgo.js
@@ -39,7 +39,7 @@ exports.isNative = isNative;
 exports.getPlatform = getPlatform;
 exports.isImmersion = isImmersion;
 exports.hasFrame = hasFrame;
-const version = '3.16.14';
+const version = '3.16.15';
 function getVersion() {
     return version;
 }
