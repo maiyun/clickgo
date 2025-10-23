0b8b50784e6554888efb745ee6f376bb593827d8
han <zohegs@gmail.com>
2025-10-23 20:05:07 +0800

    v4.1.5

--- dist/clickgo.js
+++ dist/clickgo.js
@@ -41,7 +41,7 @@ lNative.initSysId(sysId);
 export const modules = {
     'clickgo': clickgo,
 };
-const version = '4.1.4';
+const version = '4.1.5';
 /** --- 获取当前版本 --- */
 export function getVersion() {
     return version;
