c72da1d3295fc3672215eb8cbe4792245ec059ed
han <zohegs@gmail.com>
2025-11-14 13:46:20 +0800

    4.2.1

--- dist/sources/control/icon/code.js
+++ dist/sources/control/icon/code.js
@@ -6,5 +6,68 @@ export default class extends clickgo.control.AbstractControl {
             'name': '',
             'size': 's'
         };
+        this.opt = {
+            'question': {},
+            'info': {
+                'stroke': 'none',
+            },
+            'cut': {
+                'stroke': 'none',
+            },
+            'copy': {
+                'stroke': 'none',
+            },
+            'paste': {
+                'viewBox': '2 2 19 19',
+                'fill': 'none',
+            },
+            'sun': {
+                'fill': 'none',
+            },
+            'moon': {
+                'stroke': 'none',
+            },
+            'drag': {
+                'viewBox': '0 0 25 25',
+                'stroke': 'none',
+            },
+            'close': {
+                'stroke': 'none',
+            },
+            'keyboard': {
+                'fill': 'none',
+            },
+            'eye': {
+                'fill': 'none',
+            },
+            'eye-slash': {
+                'fill': 'none',
+            },
+            'refresh': {
+                'fill': 'none',
+            },
+            'trash': {
+                'fill': 'none',
+            },
+            'plus': {
+                'stroke': 'none',
+            },
+            'speaker': {
+                'fill': 'none',
+            },
+            'cpu': {
+                'fill': 'none',
+            },
+            'key': {
+                'fill': 'none',
+            },
+            'surveillance': {
+                'viewBox': '0 0 32 32',
+                'stroke': 'none',
+            },
+            'record': {
+                'fill': 'none',
+            }
+        };
     }
 }
