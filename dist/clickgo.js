4c20f53e1c4d2dbe8601b2d36791444fe6fe83fb
han <zohegs@gmail.com>
2026-03-02 13:21:16 +0800

    5.7.0

--- dist/clickgo.js
+++ dist/clickgo.js
@@ -41,7 +41,7 @@ lNative.initSysId(sysId);
 export const modules = {
     'clickgo': clickgo,
 };
-const version = '5.6.5';
+const version = '5.7.0';
 /** --- 获取当前版本 --- */
 export function getVersion() {
     return version;
