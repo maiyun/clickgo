d08e8d5a32ba56411a9605dbeae0f466a3d35263
han <zohegs@gmail.com>
2026-03-19 16:56:42 +0800

    5.12.0

--- dist/clickgo.ts
+++ dist/clickgo.ts
@@ -64,7 +64,7 @@ export const modules: {
     'clickgo': clickgo,
 } as any;
 
-const version = '5.11.1';
+const version = '5.12.0';
 /** --- 获取当前版本 --- */
 export function getVersion(): string {
     return version;
