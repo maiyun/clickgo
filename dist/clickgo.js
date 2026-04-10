788d3139c1c0d7288d717dcb20b8790984b0e612
js7y <zohegs@gmail.com>
2026-04-10 16:38:24 +0800

    5.16.2

--- dist/clickgo.js
+++ dist/clickgo.js
@@ -41,7 +41,7 @@ lNative.initSysId(sysId);
 export const modules = {
     'clickgo': clickgo,
 };
-const version = '5.16.1';
+const version = '5.16.2';
 /** --- 获取当前版本 --- */
 export function getVersion() {
     return version;
