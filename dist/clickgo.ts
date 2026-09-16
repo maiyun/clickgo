ae47102be4fa062a9cfff1728570a652d68113f9
js7y <zohegs@gmail.com>
2026-09-16 18:00:52 +0800

    6.1.4

--- dist/clickgo.ts
+++ dist/clickgo.ts
@@ -63,7 +63,7 @@ export const modules: {
     'clickgo': clickgo,
 } as any;
 
-const version = '6.1.3';
+const version = '6.1.4';
 /** --- 获取当前版本 --- */
 export function getVersion(): string {
     return version;
